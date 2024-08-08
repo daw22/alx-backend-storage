6b739a44e6ab5c17562f4767234278fe2dacff28
dawit <dawitxxd@gmail.com>
2024-08-09 00:06:22 +0300

    age

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,5 @@
--- lists all bands with Glam rock style ranked by their longevity
+-- lists all bands with `Glam rock` style ranked by their longevity
 SELECT band_name, IFNULL(split, 2020) - formed AS lifespan
 FROM metal_bands
 WHERE FIND_IN_SET('Glam rock', style)
-iORDER BY lifespan DESC;
+ORDER BY lifespan DESC;
