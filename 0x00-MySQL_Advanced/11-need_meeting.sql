b4e578735a86f66f93c1ef996b24ff14ef119eca
dawit <dawitxxd@gmail.com>
2024-08-08 21:24:16 +0300

    views

---
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -0,0 +1,8 @@
+-- creates a view 
+CREATE VIEW need_meeting AS 
+SELECT name FROM students 
+WHERE 
+    score < 80 AND 
+    (last_meeting IS NULL 
+        OR 
+    last_meeting < ADDDATE(CURDATE(), interval -1 MONTH));
