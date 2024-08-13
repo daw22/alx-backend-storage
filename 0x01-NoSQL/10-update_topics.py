0ea9e23566035be37ea51e5c0fdbefee0f0d959a
dawit <dawitxxd@gmail.com>
2024-08-13 11:41:40 +0300

    change school topics

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+pymongo update
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    updates topics of the collection
+    """
+    mongo_collection.update_one(
+            {"name": name},
+            {"$set": {"topics": topics}}
+            )
