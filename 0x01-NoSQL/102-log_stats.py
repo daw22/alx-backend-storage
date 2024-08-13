313249e3c644d8406267e306134023ee20891d2f
dawit <dawitxxd@gmail.com>
2024-08-13 20:07:06 +0300

    Log stats- new version

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -6,6 +6,9 @@ from pymongo import MongoClient
 
 
 if __name__ == "__main__":
+    """
+    nginx stats with top ips
+    """
     client = MongoClient("mongodb://127.0.0.1:27017")
     nginx_collection = client.logs.nginx
 
