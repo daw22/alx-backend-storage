71b99c8fdfd5973d7f76458a0af0fab83fb54725
dawit <dawitxxd@gmail.com>
2024-08-14 19:44:55 +0300

    write string to redis

--- 0x02-redis_basics/exercise.py
+++ 0x02-redis_basics/exercise.py
@@ -7,20 +7,20 @@ import uuid
 from typing import Union
 
 
-class Cache():
+class Cache:
     """
-    Cahche class
+    Cache class
     """
     def __init__(self):
         """
-        Inits Catche class
+        Inits Cache class
         """
         self._redis = redis.Redis()
         self._redis.flushdb(True)
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
-        Stores data to redis
+        Stores data to redis and returns the key
         """
         key = str(uuid.uuid4())
         self._redis.set(key, data)
